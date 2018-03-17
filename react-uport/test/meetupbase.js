1e449be44e121367aef77580d8abc5fa65f5f1f0
zemingyu <zemingyu@gmail.com>
2018-03-17 23:38:15 +1100

    finally fixed testing code. need multiple then

--- react-uport/test/meetupbase.js
+++ react-uport/test/meetupbase.js
@@ -1,17 +1,51 @@
+
 var MeetupBase = artifacts.require("./MeetupBase.sol");
 
 contract('MeetupBase', function(accounts) {
 
-  it("...can register user.", function() {
+  it("...can register Alice.", function() {
     return MeetupBase.deployed().then(function(instance) {
       meetupBaseInstance = instance;
 
       return meetupBaseInstance.registerUser("Alice", {from: accounts[0]});
     }).then(function() {
-      return meetupBaseInstance.get.call();
-    }).then(function(users["Alice"]) {
-      assert.equal(users["Alice"], accounts[0], "The user 'Alice' was not registered.");
+      return meetupBaseInstance.users.call("Alice")
+    }).then(function(reg_addr) {
+      assert.equal(reg_addr, accounts[0], "The user 'Alice' was not registered.");
     });
   });
 
+  it("...can register Bob.", function() {
+    return MeetupBase.deployed().then(function(instance) {
+      meetupBaseInstance = instance;
+
+      return meetupBaseInstance.registerUser("Bob", {from: accounts[1]});
+    }).then(function() {
+      return meetupBaseInstance.users.call("Bob")
+    }).then(function(reg_addr) {
+      assert.equal(reg_addr, accounts[1], "The user 'Bob' was not registered.");
+    });
+  });
+
+
 });
+
+
+
+// var MeetupBase = artifacts.require("./MeetupBase.sol");
+
+// contract('MeetupBase', function(accounts) {
+
+//   it("...can register user.", function() {
+//     return MeetupBase.deployed().then(function(instance) {
+//       meetupBaseInstance = instance;
+
+//       return meetupBaseInstance.registerUser("Alice", {from: accounts[0]});
+//     }).then(function() {
+//       return meetupBaseInstance.users.call("Alice")
+//     }).then(function(reg_addr) {
+//       assert.equal(reg_addr, accounts[0], "The user 'Alice' was not registered.");
+//     });
+//   });
+
+// });
