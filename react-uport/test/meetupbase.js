bab049750b4f80310fe861438473eb92d9da2753
Zeming Yu <zemingyu@gmail.com>
2018-03-19 22:26:02 +1100

    try to read out individual meetup info, missing presenter list and registration list. why?

--- react-uport/test/meetupbase.js
+++ react-uport/test/meetupbase.js
@@ -1,3 +1,6 @@
+// const Web3 = require('web3');
+// const web3 = new Web3(ganache.provider());
+
 
 var MeetupBase = artifacts.require("./MeetupBase.sol");
 
@@ -23,67 +26,105 @@ contract('MeetupBase', function([admin, organiser, assistant1, assistant2,
   let meetupBaseInstance;
 
   beforeEach('setup contract for each test', async function () {
+    // transfer roles
     meetupBaseInstance = await MeetupBase.new(admin);    
     await meetupBaseInstance.setOrganiser(organiser, {from: admin});    
     await meetupBaseInstance.setAssistant_1(assistant1, {from: admin});    
     await meetupBaseInstance.setAssistant_2(assistant2, {from: admin});    
+
+    // register everyone
+    await meetupBaseInstance.registerUser("Zeming Yu", {from: admin});      
+    await meetupBaseInstance.registerUser("BokkyPooBah", {from: organiser});      
+    await meetupBaseInstance.registerUser("David Lim", {from: assistant1});      
+    await meetupBaseInstance.registerUser("James Zaki", {from: assistant2});      
+    await meetupBaseInstance.registerUser("Sanjev Sunder", {from: presenter1});      
+    await meetupBaseInstance.registerUser("Andrew", {from: attendee1});      
+    await meetupBaseInstance.registerUser("Anyone", {from: anyone});      
+
+    // let balance = await web3.eth.getBalance(admin);    
+    // console.log(web3.fromWei(balance.toNumber(), 'ether'));
+
   });
 
   describe('Initial meetup setup', async () => {
-    it('has initial organiser', async () => {
+    it('has initial organiser and assistants', async () => {
       assert.equal(await meetupBaseInstance.organiserAddress(), organiser);
-    });    
-
-    it('has initial assistant1', async () => {
       assert.equal(await meetupBaseInstance.assistantAddress_1(), assistant1);
-    });    
-
-    it('has initial assistant2', async () => {
       assert.equal(await meetupBaseInstance.assistantAddress_2(), assistant2);
     });
+
   });
 
-    
-  describe('Initial meetup setup', () => {
-    it('can register Zeming Yu', async () => {
-      await meetupBaseInstance.registerUser("Zeming Yu", {from: admin});      
-      assert.equal(await meetupBaseInstance.users.call("Zeming Yu"), admin);
+  describe('Meetup events', async () => {
+    it('can creat 1 meetup event', async () => {
+      // setup an event in 1 week's time
+      beforeCount = await meetupBaseInstance.getMeetupCount();
+      await meetupBaseInstance.createMeetup(60*60*24*7, 3, [organiser, presenter1], {from: organiser});
+      afterCount = await meetupBaseInstance.getMeetupCount();
+      assert.equal(beforeCount.toNumber(), afterCount.toNumber()-1);
     });
 
-    it('can register BokkyPooBah', async () => {
-      await meetupBaseInstance.registerUser("BokkyPooBah", {from: organiser});      
-      assert.equal(await meetupBaseInstance.users.call("BokkyPooBah"), organiser);
+    it('can creat 2 meetup events', async () => {
+      // setup an event in 1 week's time and another in 2 weeks' time
+      beforeCount = await meetupBaseInstance.getMeetupCount();
+      await meetupBaseInstance.createMeetup(60*60*24*7, 3, [organiser, presenter1], {from: organiser});
+      await meetupBaseInstance.createMeetup(60*60*24*14, 3, [assistant1, assistant2], {from: assistant1});
+      afterCount = await meetupBaseInstance.getMeetupCount();
+      assert.equal(beforeCount.toNumber(), afterCount.toNumber()-2);
     });
 
-    it('can register David Lim', async () => {
-      await meetupBaseInstance.registerUser("David Lim", {from: assistant1});      
-      assert.equal(await meetupBaseInstance.users.call("David Lim"), assistant1);
-    });
+    it('allows people to join the meetup event', async () => {
+      // setup an event in 1 week's time
+      beforeCount = await meetupBaseInstance.getMeetupCount();
+      await meetupBaseInstance.createMeetup(60*60*24*7, 3, [organiser, presenter1], {from: organiser});
+      afterCount = await meetupBaseInstance.getMeetupCount();
+      assert.equal(beforeCount.toNumber(), afterCount.toNumber()-1);
 
-    it('can register James Zaki', async () => {
-      await meetupBaseInstance.registerUser("James Zaki", {from: assistant2});      
-      assert.equal(await meetupBaseInstance.users.call("James Zaki"), assistant2);
-    });
-
-    it('can register Sanjev Sunder', async () => {
-      await meetupBaseInstance.registerUser("Sanjev Sunder", {from: presenter1});      
-      assert.equal(await meetupBaseInstance.users.call("Sanjev Sunder"), presenter1);
-    });
-
-    it('can register Andrew', async () => {      
-      await meetupBaseInstance.registerUser("Andrew", {from: attendee1});      
-      assert.equal(await meetupBaseInstance.users.call("Andrew"), attendee1);
-    });
-
-    it('allows assistants to deregister Zeming Yu', async () => {
-      await meetupBaseInstance.deregisterUser("Zeming Yu", {from: organiser});      
-      assert.equal(await meetupBaseInstance.users.call("Zeming Yu"), 0x0);
+      // return the meetup struct
+      console.log(await meetupBaseInstance.meetups.call(0));
     });
   });
-
 });
 
 
+
+
+  // describe('Initial meetup setup', () => {
+  //   it('can register Zeming Yu', async () => {
+  //     await meetupBaseInstance.registerUser("Zeming Yu", {from: admin});      
+  //     assert.equal(await meetupBaseInstance.users.call("Zeming Yu"), admin);
+  //   });
+
+  //   it('can register BokkyPooBah', async () => {
+  //     await meetupBaseInstance.registerUser("BokkyPooBah", {from: organiser});      
+  //     assert.equal(await meetupBaseInstance.users.call("BokkyPooBah"), organiser);
+  //   });
+
+  //   it('can register David Lim', async () => {
+  //     await meetupBaseInstance.registerUser("David Lim", {from: assistant1});      
+  //     assert.equal(await meetupBaseInstance.users.call("David Lim"), assistant1);
+  //   });
+
+  //   it('can register James Zaki', async () => {
+  //     await meetupBaseInstance.registerUser("James Zaki", {from: assistant2});      
+  //     assert.equal(await meetupBaseInstance.users.call("James Zaki"), assistant2);
+  //   });
+
+  //   it('can register Sanjev Sunder', async () => {
+  //     await meetupBaseInstance.registerUser("Sanjev Sunder", {from: presenter1});      
+  //     assert.equal(await meetupBaseInstance.users.call("Sanjev Sunder"), presenter1);
+  //   });
+
+  //   it('can register Andrew', async () => {      
+  //     await meetupBaseInstance.registerUser("Andrew", {from: attendee1});      
+  //     assert.equal(await meetupBaseInstance.users.call("Andrew"), attendee1);
+  //   });
+
+  //   it('allows assistants to deregister Zeming Yu', async () => {
+  //     await meetupBaseInstance.deregisterUser("Zeming Yu", {from: organiser});      
+  //     assert.equal(await meetupBaseInstance.users.call("Zeming Yu"), 0x0);
+  //   });
+  // });
 
 
 // var MeetupBase = artifacts.require("./MeetupBase.sol");
